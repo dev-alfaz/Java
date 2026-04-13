2e3948fb7abd9da36f9504ed0fb82535d8cf648b
dev-alfaz <alfazhossain3272@gmail.com>
2026-04-13 15:25:34 +0600

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,5 +1,24 @@
-class Student{
+
+class Student {
+
+    String name;
+    int id;
+    double cgpa;
+
+    Student(String name, int id, double cgpa) {
+        this.name = name;
+        this.id = id;
+        this.cgpa = cgpa;
+    }
+
+    void showDetails() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Student ID: " + id);
+        System.out.println("CGPA: " + cgpa);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Student s1 = new Student("Alfaz", 74, 3.00);
+        s1.showDetails();
     }
 }
